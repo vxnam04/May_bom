ba047532039bf6b958b2a329172afaa858135478
vxnam04 <vnam79475@gmail.com>
2025-07-18 00:04:39 +0700

    first commit

---
+++ backend/WebMayBom/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Routing\Controller;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            // 'phone' => 'nullable',
+            // 'address' => 'nullable',
+            'role' => 'in:admin,user'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            // 'phone' => $request->phone,
+            // 'address' => $request->address,
+            'role' => $request->role ?? 'user',
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Sai tài khoản hoặc mật khẩu'], 401);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Đăng xuất thành công']);
+    }
+}
